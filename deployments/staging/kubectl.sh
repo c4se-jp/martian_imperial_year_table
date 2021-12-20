6d552c87c30dc5a4b9d79d1968898668018df816
ne-sachirou <ne-sachirou@hatena.ne.jp>
2021-12-20 13:05:48 +0900

    Don't log secrets

--- deployments/staging/kubectl.sh
+++ deployments/staging/kubectl.sh
@@ -6,8 +6,12 @@ curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack
 mv kustomize /usr/local/bin
 
 cd deployments/staging || exit 1
+
+set +x
 mkdir -p secret
 echo -n "$MACKEREL_APIKEY" > secret/MACKEREL_APIKEY
+set -x
+
 kustomize edit set image "gcr.io/${PROJECT_ID}/martian_imperial_year_table=gcr.io/${PROJECT_ID}/martian_imperial_year_table:${SHORT_SHA}"
 kustomize build | \
 /builder/kubectl.bash apply \
